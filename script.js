6c88aff0c0c637838c4d6ad5f25d70f8bb28ee91
sapnabilam <sapnabilam11@gmail.com>
2026-02-06 20:47:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-let messageIndex = 0;
 let messages = ["No", "Are you sure?", "Think again 😏", "Pleaseee 🥺"];
+let index = 0;
 
 function handleNoClick() {
-  const yesButton = document.querySelector(".yes-button");
-  const noButton = document.querySelector(".no-button");
+  const yesBtn = document.querySelector(".yes-button");
+  const noBtn = document.querySelector(".no-button");
 
   // change NO text
-  noButton.innerText = messages[messageIndex];
-  messageIndex = (messageIndex + 1) % messages.length;
+  noBtn.innerText = messages[index];
+  index = (index + 1) % messages.length;
 
-  // FORCE grow YES
-  let size = parseInt(yesButton.style.fontSize || 20);
-  yesButton.style.fontSize = (size + 10) + "px";
+  // grow YES button
+  let currentSize = parseInt(yesBtn.style.fontSize || 20);
+  yesBtn.style.fontSize = (currentSize + 10) + "px";
 }
 
 function sendYes() {
   window.location.href =
-    "https://wa.me/919813279342?text=I%20said%20YES%20%F0%9F%92%95";
+    "https://wa.me/919813279342?text=I%20said%20YES%20bubu%20%F0%9F%92%95";
 }
