ea5761a5507e958289754d94b19c929d5ca0c400
sapnabilam <sapnabilam11@gmail.com>
2026-02-06 20:38:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,17 @@ let messageIndex = 0;
 let messages = ["No", "Are you sure?", "Think again 😏", "Pleaseee 🥺"];
 
 function handleNoClick() {
-  const noButton = document.querySelector('.no-button');
-  const yesButton = document.querySelector('.yes-button');
+  const noButton = document.querySelector(".no-button");
+  const yesButton = document.querySelector(".yes-button");
 
+  // change text
   noButton.textContent = messages[messageIndex];
   messageIndex = (messageIndex + 1) % messages.length;
 
-  const currentSize = parseFloat(
+  // grow YES button
+  let currentSize = parseFloat(
     window.getComputedStyle(yesButton).fontSize
   );
-  yesButton.style.fontSize = `${currentSize * 1.5}px`;
-}
 
-function handleYesClick() {
-  alert("Yayyy! 💖");
+  yesButton.style.fontSize = (currentSize + 10) + "px";
 }
