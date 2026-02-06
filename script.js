637b8ce51e343231c5b7da665d67f6e4d9889522
sapnabilam <sapnabilam11@gmail.com>
2026-02-06 20:51:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ function handleNoClick() {
   const yesBtn = document.querySelector(".yes-button");
   const noBtn = document.querySelector(".no-button");
 
-  // change NO text
+  // Change NO text
   noBtn.innerText = messages[index];
   index = (index + 1) % messages.length;
 
-  // grow YES button
-  let currentSize = parseInt(yesBtn.style.fontSize || 20);
-  yesBtn.style.fontSize = (currentSize + 10) + "px";
+  // ALWAYS increase YES size
+  let size = parseInt(yesBtn.style.fontSize || 20);
+  yesBtn.style.fontSize = (size + 8) + "px";
 }
 
 function sendYes() {
   window.location.href =
-    "https://wa.me/919813279342?text=I%20said%20YES%20bubu%20%F0%9F%92%95";
+    "https://wa.me/91XXXXXXXXXX?text=I%20said%20YES%20bubu%20%F0%9F%92%95";
 }
